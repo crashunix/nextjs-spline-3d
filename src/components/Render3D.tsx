5ae1285c5dd0f1456f6eaf4b2f1a979716b6e247
Juan Gomes Macario <jgomesmacario@outlook.com>
2024-03-15 20:15:11 -0300

    first

---
+++ src/components/Render3D.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import Spline from "@splinetool/react-spline";
+
+const Render3D = () => {
+    return <Spline scene="https://prod.spline.design/REHvDHpV8e2LzcAe/scene.splinecode" />
+}
+
+export default Render3D;
